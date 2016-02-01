185fa423d2ca0f68e113d93b523c33103eff856e
Stephan Ng <stephanlunang@gmail.com>
2016-02-01 14:48:16 -0800

    Working about us and meet the lawyer.

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 if (Meteor.isClient) { 
 	angular.module('RRN', [
 		'angular-meteor',
-		'ui.router'
-		]);
+		'ui.router']);
 
   angular.module('RRN').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-
     $stateProvider
       .state('landing', {
         url: '/landing',
         templateUrl: 'landing.html'
       })
-      .state('robert', {
-        url: '/robert',
-        templateUrl: 'robert.html'
+      .state('contactus', {
+        url: '/contactus',
+        templateUrl: 'contactus.html'
+      })
+      .state('newsletter', {
+        url: '/newsletter',
+        templateUrl: 'newsletter.html'
+      })
+      .state('aboutus', {
+        url: '/aboutus',
+        templateUrl: 'aboutus.html'
+      })
+      .state('lawyer', {
+        url: '/lawyer',
+        templateUrl: 'lawyer.html'
       });
 
     $urlRouterProvider.otherwise("/landing");
   });
+
 }
